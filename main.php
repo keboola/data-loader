b6dc3a2f308fb5ce9596fdd7fb8e00d83adf64bf
Odin <odinuv@gmail.com>
2016-06-05 20:11:28 +0200

    try logger

--- main.php
+++ main.php
@@ -14,6 +14,10 @@ require "Input/Table/Manifest/Adapter.php";
 require "Input/File.php";
 require "Input/Table.php";
 
+openlog("myScriptLog", LOG_PID | LOG_PERROR, LOG_LOCAL0);
+
+syslog(LOG_WARNING, "Fantomas Comming!");
+
 $config = "
 {
     \"storage\": {
@@ -48,5 +52,8 @@ var_dump($configData);
 
 $reader->downloadFiles($configData['storage']['input']['files'], '/data');
 $reader->downloadTables($configData['storage']['input']['tables'], '/data');
+
+syslog(LOG_WARNING, "Fantomas Leaving!");
+closelog();
 
 //"{\"storage\":{\"input\":{\"tables\":[{\"source\":\"in.c-dg-main.profiles\",\"destination\":\"source.csv\",\"limit\":50},{\"source\":\"in.c-main.anvil-history\",\"destination\":\"source1.csv\",\"columns\":[\"id\",\"mpg\",\"cylinders\",\"origin\"],\"where_column\":\"origin\",\"where_values\":[1,3],\"where_operator\":\"eq\"}],\"files\":[]}}}"
