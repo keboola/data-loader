4888c5f0bbeb6e1dfe5132c74dfe5eefcfc56683
Odin <odinuv@gmail.com>
2016-06-05 19:59:20 +0200

    fix: composer install

--- Dockerfile
+++ Dockerfile
@@ -5,11 +5,14 @@ COPY . /tmp/
 WORKDIR /tmp/
 
 RUN apt-get update && apt-get install -y --no-install-recommends \
-	git && \
-	rm -r /var/lib/apt/lists/* && \
-	curl -sS --fail https://getcomposer.org/installer | php && \
-	mv /tmp/composer.phar /usr/local/bin/composer && \
-	curl -sS --fail https://s3.amazonaws.com/keboola-storage-api-cli/builds/sapi-client.0.4.0.phar > /usr/local/bin/sapi-client.phar
+	    git \
+        zlib1g-dev \
+	&& rm -r /var/lib/apt/lists/* \
+	&& docker-php-ext-install -j$(nproc) zip \
+	&& curl -sS --fail https://getcomposer.org/installer | php \
+	&& mv /tmp/composer.phar /usr/local/bin/composer \
+	&& curl -sS --fail https://s3.amazonaws.com/keboola-storage-api-cli/builds/sapi-client.0.4.0.phar > /usr/local/bin/sapi-client.phar
 
 #ENTRYPOINT php /usr/local/bin/sapi-client.phar --token=$KBC_TOKEN export-table in.c-application-testing.carseats /data/table.csv
+RUN composer install --no-interaction
 ENTRYPOINT php main.php
--- main.php
+++ main.php
@@ -38,10 +38,15 @@ $config = "
     }
 }";
 
-
+$config = getenv('KBC_EXPORT_CONFIG');
+var_dump($config);
 $client = new \Keboola\StorageApi\Client(['token' => getenv('KBC_TOKEN')]);
 $reader = new \Keboola\DockerBundle\Docker\StorageApi\Reader($client);
-$configData = json_decode(getenv('KBC_EXPORT_CONFIG'), true);
+$configData = json_decode($config, true);
+echo json_last_error_msg();
+var_dump($configData);
 
-$reader->downloadFiles($configData['storage']['input']['files'], './data');
-$reader->downloadTables($configData['storage']['input']['tables'], './data');
+$reader->downloadFiles($configData['storage']['input']['files'], '/data');
+$reader->downloadTables($configData['storage']['input']['tables'], '/data');
+
+//"{\"storage\":{\"input\":{\"tables\":[{\"source\":\"in.c-dg-main.profiles\",\"destination\":\"source.csv\",\"limit\":50},{\"source\":\"in.c-main.anvil-history\",\"destination\":\"source1.csv\",\"columns\":[\"id\",\"mpg\",\"cylinders\",\"origin\"],\"where_column\":\"origin\",\"where_values\":[1,3],\"where_operator\":\"eq\"}],\"files\":[]}}}"
