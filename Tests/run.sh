6a3e1481eb8b4ab0a2a46f02ff12161ea8da3189
Odin <odinuv@gmail.com>
2016-07-21 12:39:26 +0200

    tests: fixed paths

--- Tests/run.sh
+++ Tests/run.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
+echo "Starting tests"
 php --version \
 	&& composer --version \
- 	&& ./vendor/bin/phpcs --standard=psr2 -n --ignore=vendor --extensions=php . \
+ 	&& /code/vendor/bin/phpcs --standard=psr2 -n --ignore=vendor --extensions=php . \
 
 export KBC_EXPORT_CONFIG="{\"storage\":{\"input\":{\"tables\":[{\"source\":\"in.c-main.source\",\"destination\":\"destination.csv\"}]}}}"
 file="/data/destination.csv"
 
-php sapi-client.phar create-table --token=$KBC_TOKEN "in.c-main" "source" source.csv
-php ../main.php
+php /code/Tests/sapi-client.phar create-table --token=$KBC_TOKEN "in.c-main" "source" /code/Tests/source.csv
+php /code/main.php
+php /code/Tests/sapi-client.phar delete-table --token=$KBC_TOKEN "in.c-main.source"
 
 if [ -f "$file" ]
 then
