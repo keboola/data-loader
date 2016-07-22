456c35b4d5ed2af3b5905a7bcc3c8e48bf770454
Odin <odinuv@gmail.com>
2016-07-22 09:12:26 +0200

    fix: add latest tag

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 docker login -e="." -u="$QUAY_USERNAME" -p="$QUAY_PASSWORD" quay.io
 docker tag keboola/data-loader quay.io/keboola/data-loader:$TRAVIS_TAG
+docker tag keboola/data-loader quay.io/keboola/data-loader:latest
 docker images
 docker push quay.io/keboola/data-loader:$TRAVIS_TAG
