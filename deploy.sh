7d6fcc3b3419b452ec4d67e3cd10e59dfc0af733
Odin <odinuv@gmail.com>
2016-07-22 09:07:50 +0200

    feat: add deployment

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+docker login -e="." -u="$QUAY_USERNAME" -p="$QUAY_PASSWORD" quay.io
+docker tag keboola/data-loader quay.io/keboola/data-loader:$TRAVIS_TAG
+docker images
+docker push quay.io/keboola/data-loader:$TRAVIS_TAG
