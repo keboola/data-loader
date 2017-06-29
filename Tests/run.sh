bb0999178fec6c9171734007e77068733106b222
Odin <odinuv@gmail.com>
2017-06-29 21:30:40 +0200

    chore: fix build paths

--- Tests/run.sh
+++ Tests/run.sh
@@ -4,7 +4,7 @@ set -e
 echo "Starting tests" >&1
 php --version \
 	&& composer --version \
- 	&& /code/vendor/bin/phpcs --standard=psr2 -n --ignore=vendor --extensions=php . \
+ 	&& /code/vendor/bin/phpcs --standard=psr2 -n --ignore=vendor --extensions=php /code/
 
 export KBC_EXPORT_CONFIG="{\"storage\":{\"input\":{\"tables\":[{\"source\":\"in.c-main.source\",\"destination\":\"destination.csv\"}]}}}"
 file="/data/in/tables/destination.csv"
