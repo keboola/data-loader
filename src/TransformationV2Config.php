afdccecabadeff1d9d785cf5cd9a37db4dd9a768
pivnicek <marc@keboola.com>
2020-06-17 17:15:01 +0200

    add configuration validation for new transformations

---
+++ src/TransformationV2Config.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\DataLoader;
+
+use Keboola\InputMapping\Configuration\File;
+use Keboola\InputMapping\Configuration\Table;
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class TransformationV2Config implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder(): TreeBuilder
+    {
+        $treeBuilder = new TreeBuilder();
+        $root = $treeBuilder->root('configuration');
+        $definition = $root->children()
+            ->arrayNode('parameters')
+                ->addDefaultsIfNotSet()
+                ->ignoreExtraKeys(true)
+                ->children()
+                    ->scalarNode('type')
+                    // in future, this should be required, but for bwd compatibility is is not yet
+                        ->defaultValue('python')
+                        ->validate()
+                            ->ifNotInArray(['r', 'python', 'julia', 'test'])
+                            ->thenInvalid('Invalid sandbox type: %s. Valid values are: "r", "python", "julia", "test".')
+                        ->end()
+                    ->end()
+                    ->arrayNode('blocks')
+                        ->prototype('array')
+                            ->children()
+                                ->scalarNode('name')->end()
+                                ->arrayNode('codes')
+                                    ->prototype('array')
+                                        ->children()
+                                            ->scalarNode('name')->end()
+                                            ->arrayNode('script')
+                                                ->prototype('scalar')->end()
+                                            ->end()
+                                        ->end()
+                                    ->end()
+                                ->end()
+                            ->end()
+                        ->end()
+                    ->end()
+                ->end()
+            ->end()
+            ->arrayNode('storage')
+                ->addDefaultsIfNotSet()
+                ->children()
+                    ->arrayNode('input')
+                        ->addDefaultsIfNotSet()
+                        ->children();
+                            File::configureNode($definition->arrayNode('files')->prototype('array'));
+                            Table::configureNode($definition->arrayNode('tables')->prototype('array'));
+        return $treeBuilder;
+    }
+}
---
+++ tests/phpunit/TransformationV2ConfigTest.php
@@ -0,0 +1,182 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\DataLoader\FunctionalTests;
+
+use Keboola\DataLoader\TransformationV2Config;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Config\Definition\Exception\InvalidConfigurationException;
+use Symfony\Component\Config\Definition\Processor;
+
+class TransformationV2ConfigTest extends TestCase
+{
+    /**
+     * @dataProvider configurationProvider
+     * @param array $input
+     * @param array $expected
+     */
+    public function testSuccess(array $input, array $expected): void
+    {
+        $processor = new Processor();
+        $outData = $processor->processConfiguration(new TransformationV2Config(), ['configuration' => $input]);
+        self::assertEquals($expected, $outData);
+    }
+
+    public function configurationProvider(): array
+    {
+        return [
+            'empty config' => [
+                [],
+                [
+                    'storage' => [
+                        'input' => [
+                            'files' => [],
+                            'tables' => [],
+                        ],
+                    ],
+                    'parameters' => [
+                        'type' => 'python',
+                        'blocks' => [],
+                    ],
+                ],
+            ],
+            'full config' => [
+                [
+                    'storage' => [
+                        'input' => [
+                            'tables' => [
+                                [
+                                    'source' => 'in.c-main',
+                                    'destination' => 'my.csv',
+                                    'changed_since' => null,
+                                    'where_column' => 'a',
+                                    'where_values' => ['e', 'f'],
+                                    'where_operator' => 'eq',
+                                    'limit' => 10,
+                                    'columns' => ['x', 'y'],
+                                ],
+                            ],
+                            'files' => [
+                                [
+                                    'tags' => ['test'],
+                                ],
+                            ],
+                        ],
+                    ],
+                    'parameters' => [
+                        'type' => 'python',
+                        'blocks' => [
+                            [
+                                'name' => 'Block 1',
+                                'codes' => [
+                                    [
+                                        'name' => 'Code 1.1',
+                                        'script' => [
+                                            "print('hello code block 1')\n\nprint('end')"
+                                        ],
+                                    ],[
+                                        'name' => 'Code 1.2',
+                                        'script' => [
+                                            "print('what is this?')"
+                                        ],
+                                    ],
+                                ],
+                            ],[
+                                'name' => 'block2',
+                                'codes' => [
+                                    [
+                                        'name' => 'Code 2.1',
+                                        'script' => [
+                                            "print('this is block 2')\n\nprint('end block 2')"
+                                        ],
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+                [
+                    'storage' => [
+                        'input' => [
+                            'tables' => [
+                                [
+                                    'source' => 'in.c-main',
+                                    'destination' => 'my.csv',
+                                    'changed_since' => null,
+                                    'where_column' => 'a',
+                                    'where_values' => ['e', 'f'],
+                                    'where_operator' => 'eq',
+                                    'limit' => 10,
+                                    'columns' => ['x', 'y'],
+                                ],
+                            ],
+                            'files' => [
+                                [
+                                    'tags' => ['test'],
+                                    'limit' => 10,
+                                    'processed_tags' => [],
+                                ],
+                            ],
+                        ],
+                    ],
+                    'parameters' => [
+                        'type' => 'python',
+                        'blocks' => [
+                            [
+                                'name' => 'Block 1',
+                                'codes' => [
+                                    [
+                                        'name' => 'Code 1.1',
+                                        'script' => [
+                                            "print('hello code block 1')\n\nprint('end')"
+                                        ],
+                                    ],[
+                                        'name' => 'Code 1.2',
+                                        'script' => [
+                                            "print('what is this?')"
+                                        ],
+                                    ],
+                                ],
+                            ],[
+                                'name' => 'block2',
+                                'codes' => [
+                                    [
+                                        'name' => 'Code 2.1',
+                                        'script' => [
+                                            "print('this is block 2')\n\nprint('end block 2')"
+                                        ],
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ];
+    }
+
+    public function testInvalid(): void
+    {
+        $inData = [
+            'parameters' => [
+                'type' => 'python',
+            ],
+            'storage' => [
+                'input' => [
+                    'tables' => [
+                        [
+                            'invalidKey' => 'value',
+                        ],
+                    ],
+                ],
+            ],
+        ];
+        $processor = new Processor();
+        self::expectException(InvalidConfigurationException::class);
+        self::expectExceptionMessage(
+            'Unrecognized option "invalidKey" under "configuration.storage.input.tables.0". Available options are "changed_since", "columns", "days", "destination", "limit", "source", "where_column", "where_operator", "where_values".'
+        );
+        $processor->processConfiguration(new TransformationV2Config(), ['configuration' => $inData]);
+    }
+}
