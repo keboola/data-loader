aef2317320cd6826b2b8c3b2b7054b12c6877cf9
pivnicek <marc@keboola.com>
2020-08-14 15:55:54 +0200

    add test for variable replacement

--- tests/functional/TransformationV2SandboxTest.php
+++ tests/functional/TransformationV2SandboxTest.php
@@ -8,6 +8,7 @@ use Keboola\DatadirTests\DatadirTestSpecification;
 use Keboola\DataLoader\ScriptProcessor;
 use Keboola\StorageApi\Components;
 use Keboola\StorageApi\Options\Components\Configuration;
+use Keboola\StorageApi\Options\Components\ConfigurationRow;
 use Keboola\StorageApi\Options\Components\ListComponentConfigurationsOptions;
 use Keboola\StorageApi\Options\FileUploadOptions;
 use Keboola\StorageApi\Options\ListFilesOptions;
@@ -122,8 +123,6 @@ class TransformationV2SandboxTest extends BaseDatadirTest
         $output = $process->getOutput();
         $output = preg_replace('#\[.*?\]#', '', $output);
         $output = preg_replace('#\{.*?\}#', '', $output);
-        var_dump($output);
-        var_dump($process->getErrorOutput());
         self::assertEquals(
             implode("\n", [
                 ' app-logger.INFO: Starting Data Loader  ',
@@ -141,5 +140,124 @@ class TransformationV2SandboxTest extends BaseDatadirTest
             ]),
             $output
         );
+    }
+
+    private function createSharedConfigurations()
+    {
+        $components = new Components($this->client);
+        $configuration = new Configuration();
+        $configuration->setComponentId('keboola.variables');
+        $configuration->setName(uniqid('test-resolve-v-'));
+        $configuration->setConfiguration(
+            ['variables' => [['name' => 'firstvar', 'type' => 'string'], ['name' => 'secondvar', 'type' => 'string']]]
+        );
+        $variablesId = $components->addConfiguration($configuration)['id'];
+        $configuration->setConfigurationId($variablesId);
+        $row = new ConfigurationRow($configuration);
+        $row->setConfiguration(
+            ['values' => [['name' => 'firstvar', 'value' => 'batman'], ['name' => 'secondvar', 'value' => 'watman']]]
+        );
+        $variableValuesId = $components->addConfigurationRow($row)['id'];
+
+        $configuration = new Configuration();
+        $configuration->setComponentId('keboola.shared-code');
+        $configuration->setName(uniqid('test-resolve-sc-'));
+        $sharedCodeId = $components->addConfiguration($configuration)['id'];
+        $configuration->setConfigurationId($sharedCodeId);
+        $row = new ConfigurationRow($configuration);
+        $row->setRowId('brainfuck');
+        $row->setConfiguration(['code_content' => '++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.']);
+        $sharedCodeRowId = $components->addConfigurationRow($row)['id'];
+
+        return [$variablesId, $variableValuesId, $sharedCodeId, $sharedCodeRowId];
+    }
+
+    public function testResolveVariables()
+    {
+        $components = new Components($this->client);
+        list($variablesId, $variableValuesId, $sharedCodeId, $sharedCodeRowId) = $this->createSharedConfigurations();
+
+        $componentId = 'keboola.python-transformation-v2';
+        $configuration = new Configuration();
+        $configuration->setComponentId($componentId);
+        $configuration->setName(uniqid('test-resolve-'));
+        $configuration->setConfiguration(
+            [
+                'storage' => [],
+                'parameters' => [
+                    "packages" => ["numpy"],
+                    "blocks" => [
+                        [
+                            "name" => "abc",
+                            "codes" => [
+                                [
+                                    "name" => "a",
+                                    "script" => ["{{firstvar}}"]
+                                ],[
+                                    "name" => "b",
+                                    "script" => ["{{brainfuck}}"]
+                                ],[
+                                    "name" => "c",
+                                    "script" => ["{{secondvar}}"]
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+                'variables_id' => $variablesId,
+                'variables_values_id' => $variableValuesId,
+                'shared_code_id' => $sharedCodeId,
+                'shared_code_row_ids' => [$sharedCodeRowId]
+            ]
+        );
+        $createdConfig = $components->addConfiguration($configuration);
+        var_dump($createdConfig);
+        $envs = [
+            'KBC_TOKEN' => getenv('KBC_TEST_TOKEN'),
+            'KBC_STORAGEAPI_URL' => getenv('KBC_TEST_URL'),
+            'KBC_COMPONENT_ID' => self::COMPONENT_ID,
+            'KBC_CONFIG_ID' => $createdConfig['id'],
+            'KBC_CONFIG_VERSION' => $createdConfig['version'],
+            'KBC_VARIABLE_VALUES_ID' => $variableValuesId,
+        ];
+
+        $specification = new DatadirTestSpecification(
+            __DIR__ . '/transformationV2-fullConfig/source/data',
+            0,
+            null,
+            '',
+            null
+        );
+
+        $tempDatadir = $this->getTempDatadir($specification);
+        $process = $this->runScript($tempDatadir->getTmpFolder(), $envs);
+
+        $scriptFile = $tempDatadir->getTmpFolder() . '/notebook.ipynb';
+        $expectedScriptFile = __DIR__ . '/files/variables-notebook.ipynb';
+        self::assertEquals(
+            trim(file_get_contents($expectedScriptFile)),
+            trim(file_get_contents($scriptFile))
+        );
+
+        $output = $process->getOutput();
+        $output = preg_replace('#\[.*?\]#', '', $output);
+        $output = preg_replace('#\{.*?\}#', '', $output);
+        self::assertEquals(
+            implode("\n", [
+                ' app-logger.INFO: Starting Data Loader  ',
+                ' app-logger.INFO: DataLoader is loading data  ',
+                ' app-logger.INFO: Reading ' . self::COMPONENT_ID . ' configuration ' . $createdConfig['id'] . '  ',
+                ' app-logger.INFO: Loaded transformation script (size 118).  ',
+                ' app-logger.INFO: Found no user-defined template, using built-in.  ',
+                ' app-logger.INFO: There are no input files.  ',
+                ' app-logger.INFO: There are no input tables.  ',
+                '',
+            ]),
+            $output
+        );
+
+        $components->deleteConfiguration($componentId, $createdConfig['id']);
+        $components->deleteConfiguration('keboola.variables', $variablesId);
+        $components->deleteConfiguration('keboola.shared-code', $sharedCodeId);
     }
 }
